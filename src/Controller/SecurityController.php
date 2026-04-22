64adb64433fe15a5d3c70cae59650faf136d063b
sara <sara.burchi@gruppotfs.it>
2026-04-22 10:43:53 +0200

    login done

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -19,7 +19,7 @@ class SecurityController extends AbstractController
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('@EasyAdmin/page/login.html.twig', [
+        return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
             'csrf_token_intention' => 'authenticate',
