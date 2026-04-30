e0a67a18ac485ea62d13b735dccbac3a2515a3c7
sara <sara.burchi@gruppotfs.it>
2026-04-30 15:42:43 +0200

    ok entity

--- src/DataFixtures/OrderItemsFixtures.php
+++ src/DataFixtures/OrderItemsFixtures.php
@@ -8,12 +8,16 @@ use App\Entity\Plate;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
-class OrderItemsFixtures extends Fixture
+// Usiamo DependentFixtureInterface per essere sicuri che Order e Plate esistano già
+class OrderItemsFixtures extends Fixture 
 {
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
+        
+        // Recuperiamo gli ordini e i piatti esistenti nel DB
         $orders = $manager->getRepository(Order::class)->findAll();
         $plates = $manager->getRepository(Plate::class)->findAll();
 
@@ -22,14 +26,16 @@ class OrderItemsFixtures extends Fixture
         }
 
         foreach ($orders as $order) {
-            for ($i = 0; $i < 3; $i++) {
+            // Per ogni ordine, creiamo da 1 a 4 righe d'ordine (OrderItem)
+            $numberOfItems = rand(1, 4);
+            $randomPlates = $faker->randomElements($plates, $numberOfItems);
+
+            foreach ($randomPlates as $plate) {
                 $orderItem = new OrderItem();
                 $orderItem->setRelatedOrder($order);
-
-                $randomPlates = $faker->randomElements($plates, 3);
-                foreach ($randomPlates as $plate) {
-                    $orderItem->addPlate($plate);
-                }
+                $orderItem->setPlate($plate);
+                // Se hai aggiunto la quantità nell'entity, setta anche quella:
+                // $orderItem->setQuantity(rand(1, 3)); 
 
                 $manager->persist($orderItem);
             }
