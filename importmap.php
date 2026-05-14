40dc21963af03671297bd9cbc400a2c5c8056542
sara <sara.burchi@gruppotfs.it>
2026-05-14 10:03:24 +0200

    now the dashboard page is working, need to fix the chart bc is empty

--- importmap.php
+++ importmap.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * Returns the importmap for this application.
+ *
+ * - "path" is a path inside the asset mapper system. Use the
+ *     "debug:asset-map" command to see the full list of paths.
+ *
+ * - "entrypoint" (JavaScript only) set to true for any module that will
+ *     be used as an "entrypoint" (and passed to the importmap() Twig function).
+ *
+ * The "importmap:require" command can be used to add new entries to this file.
+ */
 return [
     'app' => [
         'path' => './assets/app.js',
@@ -16,10 +27,10 @@ return [
         'version' => '7.3.0',
     ],
     'chart.js' => [
-        'version' => '4.4.1',
+        'version' => '4.5.1',
     ],
     '@symfony/ux-chartjs' => [
-        'path' => './vendor/symfony/ux-chartjs/assets/dist/chart_controller.js',
+        'version' => '2.35.0',
     ],
     '@kurkle/color' => [
         'version' => '0.3.4',
